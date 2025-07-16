fa0aeb903a9a12bc2c0d9a11b41dde796e020172
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-15 21:51:00 -0400

    add Lab7-A

---
+++ Labs/Lab7/unindent.cpp
@@ -0,0 +1,40 @@
+/**********
+Assignment: Lab7-A
+
+Write a program called unindent.cpp, that reads 
+input from cin and prints out each input line with 
+leading spaces removed.
+
+Make use of the function isspace defined in <cctype> 
+to check if a character is a whitespace.
+**********/
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+/*
+    @line:      one line of code as input
+    @return:    code line without leading spaces and tabs
+*/
+string removeLeadingSpaces(string line) {
+    int len = line.length(); //get length
+    string fix = line;
+    int i;
+
+    for (i = 0; i < len; i++) {
+        if(!(isspace(fix[i]))) { //loop till get non space
+            break;
+        }
+    }
+
+    return fix.substr(i, len); //return first non space character onward
+}
+
+int main() {
+    cout << "Unindented file:\n";
+    string code;
+    while(getline(cin, code)) {
+        cout << removeLeadingSpaces(code) << endl;
+    }
+}
