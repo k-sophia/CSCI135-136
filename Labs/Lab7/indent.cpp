967e0ba1540e806f07955f784c183ade5073c112
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-17 15:46:47 -0400

    Lab7-B edit and comments

--- Labs/Lab7/indent.cpp
+++ Labs/Lab7/indent.cpp
@@ -1,11 +1,34 @@
+/**********
+Assignment: Lab7-B
+
+Write a new program, indent.cpp, that can fix 
+indentation in C/C++ source code files.
+
+Write a function:
+-   int countChar(string line, char c);
+
+As it reads the input line by line, it should also 
+- count the number of open and closed { } in it 
+- keep track of how many blocks is currently open at 
+    the beginning of each line
+
+Shortcomings of our program:
+- No support for loops and if statements that don’t have curly braces
+- Symbols {, } inside string and char literals are misinterpreted as blocks
+**********/
+
 #include <iostream>
 #include <cctype>
 using namespace std;
 
+/*
+    @line:      one line of code as input
+    @return:    code line without leading spaces and tabs
+*/
 string removeLeadingSpaces(string line) {
     int len = line.length(); //get length
     string fix = line;
-    int i;// new var
+    int i;
 
     for (i = 0; i < len; i++) {
         if(!(isspace(fix[i]))) { //loop till get non space
@@ -13,45 +36,40 @@ string removeLeadingSpaces(string line) {
         }
     }
 
-    return fix.substr(i, len); //return first onpsace onward
+    return fix.substr(i, len); //return first non space character onward
 }
 
+/*
+    @line:      one line of code as input
+    @return:    the number of occurrences of the character c
+*/
 int countChar(string line, char c) {
     int len = line.length(); //get length
     string fix = line;
-    int space = 0;
+    int count = 0;
 
     for (int i = 0; i < len; i++) {
-        if(fix[i] == c) { //loop till get non space
-            space++;
+        if(fix[i] == c) {
+            count++;
         }
     }
 
-    return space;
+    return count;
 }
 
 int main() {
-    string code; //make var
+    string code;
     int indent = 0;
     
     while(getline(cin, code)) { //go to each line
-        code = removeLeadingSpaces(code); //get code no pre space
-
-        if(code[0] == '}') { //minus if end
-            indent--; //decrease
-        }
-
-        for (int i = 0; i < indent; i++) { //tabs
+        code = removeLeadingSpaces(code); //get code with leading spaces removed
+        
+        indent -= countChar(code, '}'); //remove indent
+        for (int i = 0; i < indent; i++) { //print tabs
             cout << "\t";
         }
 
         cout << code << endl; //print fix code
-
-        indent += countChar(code, '{') - countChar(code, '}');
-
-        if(code[0] == '}') { //minus if end
-            indent++; //decrease
-        }
-        
+        indent += countChar(code, '{'); //add indent
     }   
 }
