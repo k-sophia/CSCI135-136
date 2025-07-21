5f2826443a713fe199c2ec2dc508d7a43534d5af
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-21 17:08:18 -0400

    add comment and rename variables

--- Projects/Project2/hamming.cpp
+++ Projects/Project2/hamming.cpp
@@ -1,3 +1,28 @@
+/**********
+Assignment: Project2-C
+
+Write a program called hamming.cpp that calculates 
+the Hamming distance between two strings. 
+
+Given two strings of equal length, the Hamming 
+distance is the number of positions at which the 
+two strings differ. 
+e.g.: Hamming("aactgc", "atcaga") would output 3
+
+File mutations.txt 
+- Contains an even number of lines (zero-indexed) 
+- The even-numbered lines contain the original DNA sequence 
+- The odd-numbered lines contain that same sequence with substitution mutations 
+
+For each pair in mutations.txt, output the Hamming 
+distance followed by “yes” or “no” whether the 
+substitution caused a change in structure.
+
+Remember that translation to proteins 
+- Starts at and includes with the first "Start" codon
+- Ends at and does not include the first "Stop" codon
+**********/
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,24 +30,35 @@
 #include <climits>
 using namespace std;
 
+/*
+    read from a file that has two fields per line 
+    where the delimiter is a space
+
+    @&dict      ifstream reference 
+    @return     reset the file pointer to the beginning for each look up
+*/
 string dictionary_read(ifstream &dict, string strand) {
     string codon, amino_acid, out;
-    dict.clear(); // reset error state
-    dict.seekg(0); // return file pointer to the AUG MET
+    dict.clear(); //reset error state
+    dict.seekg(0); //return file pointer to the beginning
     while (dict >> codon >> amino_acid) {
         if (codon == strand) {
-            out = amino_acid;
+            return amino_acid;
         }
     }
 
     return out;
 }
 
-char DNAbase_to_mRNAbase(char dna) { //convert bases dna to rna
+/*
+    @return:    the mRNA base complement of a DNA base
+*/
+char DNAbase_to_mRNAbase(char dna) {
     char DNA = toupper(dna); //make upper case
     char rna;
 
-    if (DNA == 'A') { //find compliment 
+    //find compliment
+    if (DNA == 'A') {
         rna = 'U';
     }
     else if (DNA == 'T') {
@@ -36,22 +72,27 @@ char DNAbase_to_mRNAbase(char dna) { //convert bases dna to rna
     }
 
     return rna; //return rna base char
-
 }
 
+/*
+    @return:    the mRNA strand compliment of a DNA strand
+*/
 string DNA_to_mRNA(string DNA) {
     string RNA = "";
     string dna = DNA;
-    int strand = DNA.length(); //lenght of each strand
+    int strand = DNA.length(); //length of each strand
 
     for (int i = 0; i < strand; i++) { //loop for each char in strand
-        RNA += DNAbase_to_mRNAbase(dna[i]);
+        RNA += DNAbase_to_mRNAbase(dna[i]); //get rna char compliment of dna char
     }
     
-    return RNA; //return RNA string
+    return RNA; //return RNA strand
 }
 
-string amino_acid(ifstream &dict ,string DNA) {
+/*
+    @return:    corresponding amino-acid chain of DNA strand
+*/
+string amino_acid(ifstream &dict, string DNA) {
     string amino, result;
     int len = DNA.length();
     bool start = false;
@@ -59,7 +100,7 @@ string amino_acid(ifstream &dict ,string DNA) {
     for (int i = 0; i < len; i = i +3) { //3 at time
         amino = dictionary_read(dict, DNA.substr(i,3));
 
-        if(!start and amino == "Met") {
+        if(!start and amino == "Met") { //start translation at the first AUG
             start = true;
             result += amino;
             continue;
@@ -76,21 +117,23 @@ string amino_acid(ifstream &dict ,string DNA) {
     return result;
 }
 
+/*
+    @return:    the hamming distance of two DNA strand
+*/
 int Hamming(string strand_1, string strand_2) {
-    int count = 0;
     int len = strand_1.length();
+    int distance = 0;
 
     for (int i = 0; i < len; i ++) {
         if(strand_1[i] != strand_2[i]) {
-            count++;
+            distance++;
         }
     }
 
-    return count;
+    return distance;
 }
 
 int main() {
-
     ifstream fin("mutations.txt");
     ifstream dict("codons.tsv");
     if (fin.fail() or dict.fail()) { //if either fail
@@ -99,31 +142,27 @@ int main() {
     }
 
     string strand;
-    string before, b_amino, after, a_amino;
+    while(getline(fin, strand)) { //gets even line of original DNA sequence 
+        string original_strand = strand;
+        string original_amino = amino_acid(dict, DNA_to_mRNA(original_strand));
 
-    while(getline(fin, strand)) { //goes to each strand
+        cout << "Hamming distance:\n";
+        while(getline(fin, strand)) { //gets odd line of same sequence with substitution mutations
+            string mutation_strand = strand;
+            string mutation_amino = amino_acid(dict, DNA_to_mRNA(mutation_strand));
 
-        before = strand;
-        b_amino = amino_acid(dict, DNA_to_mRNA(before));
+            cout << Hamming(original_strand, mutation_strand); //get hamming distance
 
-        while(getline(fin, strand)) { //get next
-            after = strand;
-            a_amino = amino_acid(dict, DNA_to_mRNA(after));
-
-            cout << Hamming(before, after);
-
-            if(b_amino != a_amino) {
-                cout << " yes";
+            //check change in structure
+            if(original_amino != mutation_amino) {
+                cout << " yes" << endl;
             }
-            else if (b_amino == a_amino) {
-                cout << " no";
+            else if (original_amino == mutation_amino) {
+                cout << " no" << endl;
             }
         }
-
-
     }
 
     fin.close();
     dict.close();
-
 }
