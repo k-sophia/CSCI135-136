1fa042f204dfa084fd5b7b80827008dc8b4d4f6f
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-22 11:52:47 -0400

    add Lab9-B

--- Labs/Lab9/3d-space.cpp
+++ Labs/Lab9/3d-space.cpp
@@ -1,8 +1,9 @@
 /**********
-Assignment: Lab9-A
+Assignment: Lab9
 
 Write a program, 3d-space.cpp, that defines the function:
 -   double length(Coord3D *p);
+-   Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2);
 
 Have a main function that tests your code.
 **********/
@@ -30,7 +31,32 @@ double length(Coord3D *p) {
     return sqrt(pow(p->x,2) + pow(p->y,2) + pow(p->z,2));
 }
 
+/*
+    @p1, @p2:   the coordinates of two points (passed as pointers)
+    @return:    the pointer of the point that is farther away from the origin
+*/
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2) {
+    if(length(p1) > length(p2)) { //which greater distance
+        return p1;
+    }
+    return p2;
+}
+
 int main() {
+    //Test fartherFromOrigin
+    Coord3D pointP = {10, 20, 30};
+    Coord3D pointQ = {-20, 21, -22};
+
+    cout << "Address of P: " << &pointP << endl;
+    cout << "Address of Q: " << &pointQ << endl << endl;
+
+    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+   
+    cout << "ans = " << ans << endl; // So which point is farther?
+
+    /* 
+    //Test length
     Coord3D pointP = {10, 20, 30};
     cout << length(&pointP) << endl; // would print 37.4166
+    */
 }
