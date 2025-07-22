1091f7b9d7525c90d7fbd58670616529a2f61dfc
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-22 12:10:16 -0400

    add Lab9-D

---
+++ Labs/Lab9/poem.cpp
@@ -0,0 +1,32 @@
+/**********
+Assignment: Lab9 D
+
+Given program, poem.cpp, the memory is allocated 
+dynamically, but it crashes your computer, because 
+it runs out of memory really quickly.
+
+Fix this program. It should still keep creating 
+poems, but all dynamically allocated memory should 
+get deleted when it is not needed. 
+
+The program can be stopped with Ctrl+C.
+**********/
+
+#include <iostream>
+using namespace std;
+
+string * createAPoemDynamically() {
+    string *p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
+}
+
+int main() {
+    while(true) {
+        string *p;
+        p = createAPoemDynamically();
+
+        // assume that the poem p is not needed at this point
+        delete p; 
+    }
+}
