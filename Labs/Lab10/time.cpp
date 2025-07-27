7ea60ab64c8674d48c978cca20b8891d53d4474f
Kelly Camacho <ksophia.martinez@gmail.com>
2025-07-27 18:48:06 -0400

    add Lab10-C and edit comments

--- Labs/Lab10/time.cpp
+++ Labs/Lab10/time.cpp
@@ -1,10 +1,23 @@
 /**********
 Assignment: Lab10
 
-Create a program, time.cpp, that define these functions:
+Create a program named time.cpp. 
+
+Given datatypes:
+-   class Time
+-   enum Genre
+-   class Movie
+-   class Timeslot
+
+Given functions:
+-   void printTime(Time time);
+-   void printMovie(Movie m);
+
+Define these functions:
 -   int minutesSinceMidnight(Time time);
 -   int minutesUntil(Time earlier, Time later);
 -   Time addMinutes(Time time0, int min);
+-   printTimeSlot(Timeslot ts);
 
 Have a main function that tests your code.
 **********/
@@ -22,6 +35,13 @@ class Time {
 };
 
 /*
+    prints a Time object in "H:M" format (24-hour clock) 
+*/
+void printTime(Time time) {
+    cout << time.h << ":" << time.m;
+}
+
+/*
     @return:    the number of minutes from 0:00AM (midnight) until time
 */
 int minutesSinceMidnight(Time time) {
@@ -29,8 +49,8 @@ int minutesSinceMidnight(Time time) {
 }
 
 /*
-    @return:    The difference in minutes between earlier and later
-                If earlier is after later, the result will be negative
+    @return:    the difference in minutes between earlier and later
+                if earlier is after later, the result will be negative
 */
 int minutesUntil(Time earlier, Time later) {
     int minutes = later.m - earlier.m;
@@ -58,7 +78,75 @@ Time addMinutes(Time time0, int min) {
     return time0;
 }
 
+/*
+    genre enumeration for categorizing movies.
+*/
+enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
+
+/*
+    class for representing a movie
+*/
+class Movie { 
+public: 
+    string title;
+    Genre genre;     // only one genre per movie
+    int duration;    // in minutes
+};
+
+/*
+    class for representing a scheduled showing of a movie with a start time.
+*/
+class TimeSlot { 
+public: 
+    Movie movie;     // what movie
+    Time startTime;  // when it starts
+};
+
+/*
+    prints information about a Movie
+    format: "<title> <GENRE> (<duration> min)"
+*/
+void printMovie(Movie mv){
+    string g;
+    switch (mv.genre) {
+        case ACTION   : g = "ACTION"; break;
+        case COMEDY   : g = "COMEDY"; break;
+        case DRAMA    : g = "DRAMA";  break;
+        case ROMANCE  : g = "ROMANCE"; break;
+        case THRILLER : g = "THRILLER"; break;
+    }
+    cout << mv.title << " " << g << " (" << mv.duration << " min)";
+}
+
+/*
+    prints information about a TimeSlot
+    format: "<title> <GENRE> (<duration> min) [starts at H:M ,ends by H:M]"
+*/
+void printTimeSlot(TimeSlot ts) {
+    printMovie(ts.movie);
+    cout << " [starts at ";
+    printTime(ts.startTime);
+    cout << ", ends by ";
+
+    ts.startTime = addMinutes(ts.startTime, ts.movie.duration);
+    printTime(ts.startTime);
+    cout << "]" << endl;
+}
+
 int main() {
+    //test printTimeSlot
+    Movie movie1 = {"Back to the Future", COMEDY, 116};
+    Movie movie2 = {"Black Panther", ACTION, 134};
+
+    TimeSlot morning = {movie1, {9, 15}};  
+    TimeSlot daytime = {movie2, {12, 15}}; 
+    TimeSlot evening = {movie2, {16, 45}}; 
+
+    printTimeSlot(morning);
+    printTimeSlot(daytime);
+    printTimeSlot(evening);
+
+    /* 
     //test addMinutes
     Time a;
     int minutes;
@@ -69,7 +157,10 @@ int main() {
     cin >> minutes;
 
     a = addMinutes(a, minutes);
-    cout << "\nThe time is now " << a.h << ":" << a.m << ".\n";
+    cout << "\nThe time is now ";
+    printTime(a);
+    cout << endl;
+    */
 
     /* 
     // test minutesSinceMidnight and minutesUntil
