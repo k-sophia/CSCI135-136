7c82b0ee1d314a027994cff0e7a29a70907e7c49
Kelly Camacho <ksophia.martinez@gmail.com>
2025-08-03 18:32:09 -0400

    small edit and add comment to Lab11-B

--- Labs/Lab11/social2.cpp
+++ Labs/Lab11/social2.cpp
@@ -60,22 +60,22 @@ void Profile::setDisplayName(string dspn) {
 }
 
 class Network {
-private:
-    static const int MAX_USERS = 20; // max number of user profiles
-    int numUsers;                    // number of registered users
-    Profile profiles[MAX_USERS];     // user profiles array:
-                                     // mapping integer ID -> Profile
+    private:
+        static const int MAX_USERS = 20; // max number of user profiles
+        int numUsers;                    // number of registered users
+        Profile profiles[MAX_USERS];     // user profiles array:
+                                        // mapping integer ID -> Profile
 
-    // Returns user ID (index in the 'profiles' array) by their username
-    // (or -1 if username is not found)
-    int findID (string usrn);
+        // Returns user ID (index in the 'profiles' array) by their username
+        // (or -1 if username is not found)
+        int findID (string usrn);
     public:
-    // Constructor, makes an empty network (numUsers = 0)
-    Network();
+        // Constructor, makes an empty network (numUsers = 0)
+        Network();
 
-    // Attempts to sign up a new user with specified username and displayname
-    // return true if the operation was successful, otherwise return false
-    bool addUser(string usrn, string dspn);
+        // Attempts to sign up a new user with specified username and displayname
+        // return true if the operation was successful, otherwise return false
+        bool addUser(string usrn, string dspn);
 };
 
 Network::Network() {
@@ -92,7 +92,7 @@ int Network::findID(string usrn) {
 }
 
 bool Network::addUser(string usrn, string dspn) {
-    for (int i = 0; i < usrn.length(); i++) {
+    for (int i = 0; i < usrn.length(); i++) { //checks if usern is a non-empty alphanumeric string
         if(!isdigit(usrn[i]) and !isalpha(usrn[i])) {
             return false;
         }
